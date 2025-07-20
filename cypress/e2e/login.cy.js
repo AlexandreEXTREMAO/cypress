bf7c3891c54f9188fd97ef8e532b3651cf5313ca
joaoAlmir <joaoalmircos@gmail.com>
2025-07-20 19:29:46 -0300

    add commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,43 +1,15 @@
 describe('login valido',() => {
 
     it('login com sucesso', () => {
-        // Arrange
-        cy.visit('https://www.saucedemo.com/v1/index.html')
-
-        // Act
-        cy.get('[data-test="username"]').type('standard_user')
-            
-        cy.get('[data-test="password"]').type('secret_sauce')
-
-        cy.get('#login-button').click()
-
-        // Assert
-        cy.url().should('eq', 'https://www.saucedemo.com/v1/inventory.html')
+        cy.loginUserPadrão('standard_user', 'secret_sauce')
     })
 
 })
 
-describe('login invalido',() => {
+describe('login invalido', () => {
 
     it('login invalidado com sucesso', () => {
-        // Arrange
-        cy.visit('https://www.saucedemo.com/v1/index.html')
-        
-        // Act
-        cy.get('[data-test="username"]').type('Alexandre')
-            
-        cy.get('[data-test="password"]').type('123456')
-
-        cy.get('#login-button').click()
-
-        // Assert
-        cy.get('[data-test="error"]')
-        .should(
-            'contain.text',
-            'Username and password do not match any user in this service'
-        )
-
-        cy.url().should('eq', 'https://www.saucedemo.com/v1/index.html')
+        cy.loginInvalido('Alexandre', '123456')
     })
 
 })
@@ -45,24 +17,7 @@ describe('login invalido',() => {
 describe('login bloqueado',() => {
 
     it('login bloqueado com sucesso', () => {
-        // Arrange
-        cy.visit('https://www.saucedemo.com/v1/index.html')
-
-        // Act
-        cy.get('[data-test="username"]').type('locked_out_user')
-            
-        cy.get('[data-test="password"]').type('secret_sauce')
-
-        cy.get('#login-button').click()
-
-        // Assert
-        cy.get('[data-test="error"]')
-            .should(
-                'contain.text',
-                'Sorry, this user has been locked out.'
-            )
-
-        cy.url().should('eq', 'https://www.saucedemo.com/v1/index.html')
+        cy.loginUsuarioBloqueado('locked_out_user', 'secret_sauce')
     })
 
 })
